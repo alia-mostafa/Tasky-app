3fb52470da034c769b83f513800f167122e79699
Alia Mostafa <alia.mostafa142@icloud.com>
2025-09-05 00:38:41 +0300

    - add native splash screen

--- lib/feature/auth/ui/view/loginscreen.dart
+++ lib/feature/auth/ui/view/loginscreen.dart
@@ -1,10 +1,14 @@
  import 'package:flutter/material.dart';
 
 class Loginscreen extends StatelessWidget {
+  static const  routeName="Loginscreen";
+
   const Loginscreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      
+    );
   }
 }
--- lib/feature/auth/ui/view/register_screen.dart
+++ lib/feature/auth/ui/view/register_screen.dart
@@ -2,7 +2,7 @@
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
-
+static const  routeName="registerscreen";
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
